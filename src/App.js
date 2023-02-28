1f27b05d86e5cc98fc4ab1235bc57e4ed1c30f8b
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-02-28 12:55:44 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import QRCode from 'qrcode.react';
 import Form from './components/Form';
 import Qrcode from './components/Qrcode';
+import { useContext } from 'react';
+import { Store } from './Store';
+const { state, dispatch: ctxDispatch } = useContext(Store);
+const { userInfo } = state;
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Routes>
         <Route path="/" element={<Qrcode />} />
         <Route path="/signin" element={<Form />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StoreProvider>
-      {' '}
-      <App />{' '}
+      <BrowserRouter>
+        <App />{' '}
+      </BrowserRouter>{' '}
     </StoreProvider>{' '}
   </React.StrictMode>
 );
